947266ad2e68b138359e30f6984a42d22ec4ab21
Frog <ha_rezgui@esi.dz>
2019-11-27 13:51:52 +0100

    starting the backend

--- controllers/student.js
+++ controllers/student.js
@@ -1,17 +1,16 @@
 const Student = require("../models/student");
 
 exports.getStudents = (req, res) => {
-  Student.create({
-    id: 1,
-    nom: 'rezgui',
-    prenom: 'abdelkader',
-    matricule: 151515,
-    year: '1cs',
-    section: 'A',
-    group: '2'
-  })
-  .then()
-  .catch(err=>console.log(err));
+  // Student.create({
+  //   nom: 'rezgui',
+  //   prenom: 'abdelkader',
+  //   matricule: 151515,
+  //   year: '1cs',
+  //   section: 'A',
+  //   group: '2'
+  // })
+  // .then()
+  // .catch(err=>console.log(err));
   Student.findAll()
     .then(students => {
       console.log(students);
