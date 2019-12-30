364c2deb4a2368a6f67e15b52232b0db252b8279
Frog <ha_rezgui@esi.dz>
2019-12-31 00:22:14 +0100

    Added the diffrent routes for the last added functions

--- routes/acceuil.js
+++ routes/acceuil.js
@@ -6,23 +6,17 @@ const teacherController = require("../controllers/teacher");
 const router = express.Router();
 const app = express();
 
-// /admin/add-product => POST
 router.post("/add-student", studentController.postAddStudent);
-
-// /admin/students => GET
 router.get("/students", studentController.getStudents);
-
-
-//router.post('/add-student', adminController);
 router.get("/students/:studentId", studentController.getStudent);
-
-//router.post('/edit-student', adminController);
-
-//router.post('/delete-student', adminController.postDeleteProduct);
+router.get("/students/year/:studentYear", studentController.getStudentsYear);
+router.get("/students/section/:studentSection", studentController.getStudentsSection);
+router.get("/students/group/:studentGroup", studentController.getStudentsGroup);
+router.post('/delete-student', adminController.postDeleteStudent);
 
 router.get("/teachers", teacherController.getTeachers);
-
 router.get("/teachers/:teacherId", teacherController.getTeacher);
-
 router.post("/add-teacher",teacherController.postAddTeacher);
+router.post('/delete-teacher', adminController.postDeleteTeacher);
+
 module.exports = router;
